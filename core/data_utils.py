b83fb67b3128a048477def1330bac56f703766e6
chenwei <wei.chen@imotion.ai>
2020-10-16 18:31:54 +0800

    ~

--- core/data_utils.py
+++ core/data_utils.py
@@ -19,12 +19,13 @@ Introduction
 ------------
     对训练数据的ground truth box进行预处理
 Parameters
-----------
+------------
     true_boxes: ground truth box 形状为[boxes, 5], x_min, y_min, x_max, y_max, class_id
     input_shape:
     anchors:
     num_classes:
-Return
+Returns
+------------
     y_true[0]
     y_true[1]
     y_true[2]
@@ -107,17 +108,122 @@ def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes):
 """
 Introduction
 ------------
+    解析tfRecord数据
+Parameters
+------------
+    serialized_example: 序列化的每条数据
+Returns
+------------
+
+
+"""
+def parse_serialized(serialized_example):
+    features = tf.parse_single_example(
+        serialized_example,
+        features={
+            'image/encoded': tf.FixedLenFeature([], dtype=tf.string),
+            'image/object/bbox/xmin': tf.VarLenFeature(dtype=tf.float32),
+            'image/object/bbox/xmax': tf.VarLenFeature(dtype=tf.float32),
+            'image/object/bbox/ymin': tf.VarLenFeature(dtype=tf.float32),
+            'image/object/bbox/ymax': tf.VarLenFeature(dtype=tf.float32),
+            'image/object/bbox/label': tf.VarLenFeature(dtype=tf.float32)
+        }
+    )
+    image = tf.image.decode_jpeg(features['image/encoded'], channels=3)
+    image = tf.image.convert_image_dtype(image, tf.uint8)
+    xmin = tf.expand_dims(features['image/object/bbox/xmin'].values, axis=0)
+    ymin = tf.expand_dims(features['image/object/bbox/ymin'].values, axis=0)
+    xmax = tf.expand_dims(features['image/object/bbox/xmax'].values, axis=0)
+    ymax = tf.expand_dims(features['image/object/bbox/ymax'].values, axis=0)
+    label = tf.expand_dims(features['image/object/bbox/label'].values, axis=0)
+    bbox = tf.concat(axis=0, values=[xmin, ymin, xmax, ymax, label])
+    bbox = tf.transpose(bbox, [1, 0])
+
+    image, bbox = data_preprocess(image, bbox)
+    bbox_true_13, bbox_true_26, bbox_true_52 = tf.py_func(preprocess_true_boxes, [bbox], [tf.float32, tf.float32, tf.float32])
+
+    return image, bbox, bbox_true_13, bbox_true_26, bbox_true_52
+
+
+"""
+Introduction
+------------
+    对图片进行预处理，增强数据集
+Parameters
+------------
+    image: tensorflow解析的图片
+    bbox: 图片中对应的box坐标
+"""
+def data_preprocess(image, bbox):
+    image_width, image_high = tf.cast(tf.shape(image)[1], tf.float32), tf.cast(tf.shape(image)[0], tf.float32)
+    input_width = tf.cast(self.input_shape, tf.float32)
+    input_high = tf.cast(self.input_shape, tf.float32)
+    new_high = image_high * tf.minimum(input_width / image_width, input_high / image_high)
+    new_width = image_width * tf.minimum(input_width / image_width, input_high / image_high)
+    # 将图片按照固定长宽比进行padding缩放
+    dx = (input_width - new_width) / 2
+    dy = (input_high - new_high) / 2
+    image = tf.image.resize_images(image, [tf.cast(new_high, tf.int32), tf.cast(new_width, tf.int32)],
+                                   method=tf.image.ResizeMethod.BICUBIC)
+    new_image = tf.image.pad_to_bounding_box(image, tf.cast(dy, tf.int32), tf.cast(dx, tf.int32),
+                                             tf.cast(input_high, tf.int32), tf.cast(input_width, tf.int32))
+    image_ones = tf.ones_like(image)
+    image_ones_padded = tf.image.pad_to_bounding_box(image_ones, tf.cast(dy, tf.int32), tf.cast(dx, tf.int32),
+                                                     tf.cast(input_high, tf.int32), tf.cast(input_width, tf.int32))
+    image_color_padded = (1 - image_ones_padded) * 128
+    image = image_color_padded + new_image
+    # 矫正bbox坐标
+    xmin, ymin, xmax, ymax, label = tf.split(value=bbox, num_or_size_splits=5, axis=1)
+    xmin = xmin * new_width / image_width + dx
+    xmax = xmax * new_width / image_width + dx
+    ymin = ymin * new_high / image_high + dy
+    ymax = ymax * new_high / image_high + dy
+    bbox = tf.concat([xmin, ymin, xmax, ymax, label], 1)
+    if self.mode == 'train':
+        # 随机左右翻转图片
+        def _flip_left_right_boxes(boxes):
+            xmin, ymin, xmax, ymax, label = tf.split(value=boxes, num_or_size_splits=5, axis=1)
+            flipped_xmin = tf.subtract(input_width, xmax)
+            flipped_xmax = tf.subtract(input_width, xmin)
+            flipped_boxes = tf.concat([flipped_xmin, ymin, flipped_xmax, ymax, label], 1)
+            return flipped_boxes
+
+        flip_left_right = tf.greater(tf.random_uniform([], dtype=tf.float32, minval=0, maxval=1), 0.5)
+        image = tf.cond(flip_left_right, lambda: tf.image.flip_left_right(image), lambda: image)
+        bbox = tf.cond(flip_left_right, lambda: _flip_left_right_boxes(bbox), lambda: bbox)
+    # 将图片归一化到0和1之间
+    image = image / 255.
+    image = tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+    bbox = tf.clip_by_value(bbox, clip_value_min=0, clip_value_max=input_width - 1)
+    bbox = tf.cond(tf.greater(tf.shape(bbox)[0], config.max_boxes), lambda: bbox[:config.max_boxes],
+                   lambda: tf.pad(bbox, paddings=[[0, config.max_boxes - tf.shape(bbox)[0]], [0, 0]], mode='CONSTANT'))
+    return image, bbox
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Introduction
+------------
     建立数据集dataset
 Parameters
-----------
+------------
     batch_size: batch大小
-Return
-------
+Returns
+------------
     dataset: 返回tensorflow的dataset
 """
-def build_dataset(self, batch_size):
+def build_dataset(batch_size):
     dataset = tf.data.TFRecordDataset(filenames=self.TfrecordFile)
-    dataset = dataset.map(self.parser, num_parallel_calls=10)
+    dataset = dataset.map(parse_serialized, num_parallel_calls=10)
     if self.mode == 'train':
         dataset = dataset.repeat().shuffle(9000).batch(batch_size).prefetch(batch_size)
     else:
